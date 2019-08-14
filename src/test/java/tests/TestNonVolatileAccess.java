8fbb36d24d0e8ba5cc61716da843e02ec1c15ca2
Christian Tischer <christian.tischer@embl.de>
2019-08-14 09:17:56 +0200

    Add nonVolatileAccess test

--- src/test/java/tests/TestNonVolatileAccess.java
+++ src/test/java/tests/TestNonVolatileAccess.java
@@ -1,19 +1,21 @@
-package playground;
+package tests;
 
 import de.embl.cba.bdv.utils.BdvUtils;
 import de.embl.cba.bdv.utils.sources.LazySpimSource;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.RealType;
+import org.junit.Test;
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
 
-public class TryNonVolatileAccess
+public class TestNonVolatileAccess
 {
-	public static < R extends RealType< R > >  void main( String[] args )
+	@Test
+	public < R extends RealType< R > > void lazySpimSourceAccess()
 	{
 		final LazySpimSource< R > source = new LazySpimSource<>( "source",
-				TryNonVolatileAccess.class.getResource( "../labels-ulong.xml" ).getFile() );
+				TestNonVolatileAccess.class.getResource( "../test-data/labels-ulong.xml" ).getFile() );
 
 		final RandomAccessibleInterval< ? extends RealType< ? > > rai =
 				BdvUtils.getRealTypeNonVolatileRandomAccessibleInterval( source, 0, 0 );
@@ -22,5 +24,12 @@ public class TryNonVolatileAccess
  		randomAccess.setPosition( new long[]{10,10,10} );
 		final RealType< ? > realType = randomAccess.get();
 		System.out.println( realType.getRealDouble() );
+		assertEquals( 2.0, realType.getRealDouble(), 0.0 );
 	}
+
+	public static void main( String[] args )
+	{
+		new TestNonVolatileAccess().lazySpimSourceAccess( );
+	}
+
 }
