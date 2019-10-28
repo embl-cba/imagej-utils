fa976c3aed3da48c9d45cc4ba1f04ab89c2c209e
Christian Tischer <christian.tischer@embl.de>
2019-10-28 17:51:32 +0100

    Add test: XML/HDF5 export

--- src/main/java/de/embl/cba/bdv/utils/command/BdvTiffPlanesWriterCommand.java
+++ src/main/java/de/embl/cba/bdv/utils/command/BdvTiffPlanesWriterCommand.java
@@ -1,9 +1,6 @@
 package de.embl.cba.bdv.utils.command;
 
-import bdv.export.ExportMipmapInfo;
-import bdv.ij.export.imgloader.ImagePlusImgLoader.MinMaxOption;
 import de.embl.cba.bdv.utils.io.BdvImagePlusExport;
-import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.FolderOpener;
 import org.scijava.command.Command;
@@ -18,38 +15,35 @@ import java.io.File;
 public class BdvTiffPlanesWriterCommand implements Command
 {
 	@Parameter( label = "Input folder", style = "directory, open" )
-	File inputFolder;
+	public File inputFolder;
 
 	@Parameter( label = "Output file path (.xml)", style = "save" )
-	File xmlOutputPath;
+	public File xmlOutputPath;
 
 	@Parameter( label = "Voxel unit" )
-	String voxelUnit = "micrometer";
+	public String voxelUnit = "micrometer";
 
 	@Parameter( label = "Voxel size X" )
-	Double voxelSizeX;
+	public Double voxelSizeX;
 
 	@Parameter( label = "Voxel size Y" )
-	Double voxelSizeY;
+	public Double voxelSizeY;
 
 	@Parameter( label = "Voxel size Z" )
-	Double voxelSizeZ;
+	public Double voxelSizeZ;
 
 	@Override
 	public void run()
 	{
-		if ( ij.Prefs.setIJMenuBar )
-			System.setProperty( "apple.laf.useScreenMenuBar", "true" );
-
-		final ImagePlus imp = getImagePlus();
+		final ImagePlus imp = openVirtualImagePlus();
 		BdvImagePlusExport.saveAsBdv( imp, xmlOutputPath );
 	}
 
-	public ImagePlus getImagePlus()
+	private ImagePlus openVirtualImagePlus()
 	{
 		final FolderOpener folderOpener = new FolderOpener();
 		folderOpener.openAsVirtualStack( true );
-		ImagePlus imp = folderOpener.openFolder( inputFolder.getParent() );
+		ImagePlus imp = folderOpener.openFolder( inputFolder.getAbsolutePath() );
 
 		imp.getCalibration().pixelWidth = voxelSizeX;
 		imp.getCalibration().pixelHeight = voxelSizeY;
@@ -58,88 +52,4 @@ public class BdvTiffPlanesWriterCommand implements Command
 
 		return imp;
 	}
-
-	protected static class Parameters
-	{
-		final boolean setMipmapManual;
-
-		final int[][] resolutions;
-
-		final int[][] subdivisions;
-
-		final File seqFile;
-
-		final File hdf5File;
-
-		final MinMaxOption minMaxOption;
-
-		final double rangeMin;
-
-		final double rangeMax;
-
-		final boolean deflate;
-
-		final boolean split;
-
-		final int timepointsPerPartition;
-
-		final int setupsPerPartition;
-
-		public Parameters(
-				final boolean setMipmapManual, final int[][] resolutions, final int[][] subdivisions,
-				final File seqFile, final File hdf5File,
-				final MinMaxOption minMaxOption, final double rangeMin, final double rangeMax, final boolean deflate,
-				final boolean split, final int timepointsPerPartition, final int setupsPerPartition )
-		{
-			this.setMipmapManual = setMipmapManual;
-			this.resolutions = resolutions;
-			this.subdivisions = subdivisions;
-			this.seqFile = seqFile;
-			this.hdf5File = hdf5File;
-			this.minMaxOption = minMaxOption;
-			this.rangeMin = rangeMin;
-			this.rangeMax = rangeMax;
-			this.deflate = deflate;
-			this.split = split;
-			this.timepointsPerPartition = timepointsPerPartition;
-			this.setupsPerPartition = setupsPerPartition;
-		}
-	}
-
-
-	protected Parameters getParametersAutomated( final int bitDepth,
-												 final ExportMipmapInfo autoMipmapSettings,
-												 String xmlExportPath )
-	{
-
-		String seqFilename = xmlExportPath;
-
-		final String hdf5Filename = xmlExportPath.substring( 0, seqFilename.length() - 4 ) + ".h5";
-		final File hdf5File = new File( hdf5Filename );
-
-		if ( !seqFilename.endsWith( ".xml" ) ) seqFilename += ".xml";
-		final File seqFile = new File( seqFilename );
-		final File parent = seqFile.getParentFile();
-		if ( parent == null || ! parent.exists() || ! parent.isDirectory() )
-		{
-			IJ.showMessage( "Invalid export filename " + seqFilename );
-			return null;
-		}
-
-		final Parameters parameters = new Parameters( false,
-				autoMipmapSettings.getExportResolutions(),
-				autoMipmapSettings.getSubdivisions(),
-				seqFile,
-				hdf5File,
-				MinMaxOption.SET,
-				0,
-				Math.pow( 2, bitDepth ) - 1.0 ,
-				true,
-				false,
-				0,
-				0 );
-
-		return parameters;
-	}
-
 }
---
+++ src/test/java/tests/TestBdvTiffSlicesExportCommand.java
@@ -0,0 +1,33 @@
+package tests;
+
+import bdv.util.BdvFunctions;
+import de.embl.cba.bdv.utils.BdvUtils;
+import de.embl.cba.bdv.utils.command.BdvTiffPlanesWriterCommand;
+import mpicbg.spim.data.SpimData;
+import org.junit.Test;
+
+import java.io.File;
+
+public class TestBdvTiffSlicesExportCommand
+{
+	@Test
+	public void run()
+	{
+		final BdvTiffPlanesWriterCommand command = new BdvTiffPlanesWriterCommand();
+
+		command.inputFolder = new File( getClass().getResource( "../test-data/tiff-slices" ).getFile() );
+		command.voxelSizeX = 1.0;
+		command.voxelSizeY = 1.0;
+		command.voxelSizeZ = 20.0;
+		command.xmlOutputPath = new File("/Users/tischer/Documents/bdv-utils/src/test/resources/test-data/mri.xml" );
+
+		command.run();
+
+		BdvFunctions.show( BdvUtils.openSpimData( command.xmlOutputPath.getAbsolutePath() ) );
+	}
+
+	public static void main( String[] args )
+	{
+		new TestBdvTiffSlicesExportCommand().run();
+	}
+}
