9d6de376065f77d50281e776716941cbb092c699
Christian Tischer <christian.tischer@embl.de>
2020-06-03 13:08:53 +0200

    Add code from plateViewer

---
+++ src/main/java/de/embl/cba/swing/PopupMenu.java
@@ -0,0 +1,35 @@
+package de.embl.cba.swing;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class PopupMenu
+{
+	private JPopupMenu popup;
+
+	public PopupMenu(  )
+	{
+		createPopupMenu();
+	}
+
+	private void createPopupMenu()
+	{
+		popup = new JPopupMenu();
+	}
+
+	private void addPopupLine() {
+		popup.addSeparator();
+	}
+
+	public void addPopupAction( String actionName, ActionListener actionListener ) {
+
+		JMenuItem menuItem = new JMenuItem(actionName);
+		menuItem.addActionListener( actionListener );
+		popup.add(menuItem);
+	}
+
+	public void show( JComponent display, int x, int y )
+	{
+		popup.show( display, x, y );
+	}
+}
