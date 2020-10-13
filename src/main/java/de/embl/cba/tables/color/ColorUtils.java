bfca3e1584a69cbc4f63512065acfd06d56ad3c2
Kimberly Meechan <kimberly.meechan@embl.de>
2020-10-13 15:31:07 +0100

    allow parsing of ARGBType strings

--- src/main/java/de/embl/cba/tables/color/ColorUtils.java
+++ src/main/java/de/embl/cba/tables/color/ColorUtils.java
@@ -31,6 +31,8 @@ package de.embl.cba.tables.color;
 import net.imglib2.type.numeric.ARGBType;
 
 import java.awt.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class ColorUtils
 {
@@ -54,7 +56,17 @@ public abstract class ColorUtils
 		try {
 			return (Color)Color.class.getField(name.toUpperCase()).get(null);
 		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-			return null;
+			Color color = null;
+			// assume of form from ARGBType.toString() i.e. "(r=255,g=255,b=255,a=255)"
+			Pattern pattern = Pattern.compile("\\(r=(.+),g=(.+),b=(.+),a=(.+)\\)");
+			Matcher matcher = pattern.matcher(name);
+			while(matcher.find()) {
+				color = new Color(Integer.parseInt(matcher.group(1)),
+						Integer.parseInt(matcher.group(2)),
+						Integer.parseInt(matcher.group(3)),
+						Integer.parseInt(matcher.group(4)));
+			}
+			return color;
 		}
 	}
 
