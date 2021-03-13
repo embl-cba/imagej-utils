e9503b80561e6e5904f62a926a96e04f8448433c
Constantin Pape <c.pape@gmx.net>
2021-03-13 22:20:01 +0100

    Determine if bucket needs credentials dynamically

--- src/main/java/de/embl/cba/tables/S3Utils.java
+++ src/main/java/de/embl/cba/tables/S3Utils.java
@@ -1,12 +1,16 @@
 package de.embl.cba.tables;
 
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.*;
+import com.google.api.client.http.HttpStatusCodes;
 import ij.gui.GenericDialog;
 
 import java.io.File;
@@ -17,20 +21,66 @@ import java.util.stream.Collectors;
 
 public abstract class S3Utils {
 
+    // TODO if we decide to use this, we should get rid of S3CredentialsCreator and use this method in mobie
+    // then we can also get rid of the authentication field in the bdv.xml
     public static AmazonS3 getS3Client( String uri ) {
         final String endpoint = getEndpoint( uri );
         final String region = "us-west-2";
         final AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(endpoint, region);
 
-        // final S3CredentialsCreator.S3Authentication authentication = S3CredentialsCreator.S3Authentication.Anonymous;
-        final S3CredentialsCreator.S3Authentication authentication = S3CredentialsCreator.S3Authentication.Protected;
+        final String[] bucketAndObject = getBucketAndObject(uri);
+
+        // first we create a client with anon credentials and see if we can access the bucket like this
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider( new AnonymousAWSCredentials() );
         AmazonS3 s3 = AmazonS3ClientBuilder
                 .standard()
                 .withPathStyleAccessEnabled(true)
                 .withEndpointConfiguration(endpointConfiguration)
-                .withCredentials(S3CredentialsCreator.getCredentialsProvider(authentication))
+                .withCredentials(credentialsProvider)
                 .build();
-        return s3;
+
+        // check if we can access the access
+        HeadBucketRequest headBucketRequest = new HeadBucketRequest(bucketAndObject[0]);
+        try {
+            HeadBucketResult headBucketResult = s3.headBucket(headBucketRequest);
+            return s3;
+        }
+        catch (AmazonServiceException e) {
+            switch(e.getStatusCode()) {
+                // if we get a 403 response (access forbidden), we try again with credentials
+                case HttpStatusCodes.STATUS_CODE_FORBIDDEN:
+                    credentialsProvider = new DefaultAWSCredentialsProviderChain();
+                    checkCredentialsExistence(credentialsProvider);
+                    s3 = AmazonS3ClientBuilder
+                            .standard()
+                            .withPathStyleAccessEnabled(true)
+                            .withEndpointConfiguration(endpointConfiguration)
+                            .withCredentials(credentialsProvider)
+                            .build();
+                    // check if we have access permissions now
+                    try {
+                        HeadBucketResult headBucketResult = s3.headBucket(headBucketRequest);
+                    } catch(AmazonServiceException e2) {
+                        throw e2;
+                    }
+                    return s3;
+                    // otherwise the bucket does not exist or has been permanently moved; throw the exception
+                default:
+                    throw e;
+            }
+        }
+    }
+
+    public static void checkCredentialsExistence( AWSCredentialsProvider credentialsProvider )
+    {
+        try
+        {
+            credentialsProvider.getCredentials();
+        }
+        catch ( Exception e )
+        {
+            throw  new RuntimeException( e ); // No credentials could be found
+        }
     }
 
     public static String[] getBucketAndObject( String uri ) {
